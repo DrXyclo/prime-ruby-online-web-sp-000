387768c093aaab8099023303930f7af02d610fb9
Dr. XyClo <sdcarmody@gmail.com>
2019-01-06 01:20:34 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -11,6 +11,42 @@ require 'pry'
 
 
 
+
+
+
+
+
+
+
+def prime?(integer)
+  if int.is_between
+
+
+
+
+
+
+
+
+
+describe "prime?" do
+  it 'returns true for prime numbers' do
+    expect(prime?(2)).to be(true)
+    expect(prime?(3)).to be(true)
+    expect(prime?(11)).to be(true)
+    expect(prime?(105557)).to be(true)
+  end
+
+  it 'returns false for non-prime numbers' do
+    expect(prime?(-1)).to be(false), "Be sure to account for negative numbers!"
+    expect(prime?(0)).to be(false)
+    expect(prime?(1)).to be(false)
+    expect(prime?(4)).to be(false)
+    expect(prime?(40)).to be(false)
+    expect(prime?(1763)).to be(false)
+    expect(prime?(101013)).to be(false)
+
+
  
 # Delete numbers divisible by known primes 
 
