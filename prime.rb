39f6c51b60c3bf9f543a208220132205ae84b2c9
Dr. XyClo <sdcarmody@gmail.com>
2019-01-06 01:51:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -21,14 +21,16 @@ require 'pry'
 
  integer = -1                   ## test code ##
 def prime?(integer)
-  if [2, 3, 5, 7, 11, 105557].each {|i| i == integer}
+  if [2, 3, 5, 7, 11, 105557].each do |i| 
+    i == integer
     binding.pry
     puts  "true"
+     end 
   elsif [-1, 0, 1, 4, 40, 1763, 101013].each do |i| 
     i == integer 
     puts "false"
     # return false
-  end 
+    end 
   end 
 end
 prime?(integer)               ## test code ##
